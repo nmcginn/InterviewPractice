6ed9eccf8c964064add2baec09eae819cff540c4
Nathan McGinn <nmcginn@users.noreply.github.com>
2018-04-09 10:30:50 -0500

    generic sort

--- InterviewPractice/Sort.cs
+++ InterviewPractice/Sort.cs
@@ -4,15 +4,15 @@ using System.Text;
 
 namespace InterviewPractice
 {
-    public class Sort<T>
+    public class Sort<T> where T: IComparable
     {
-        public static int[] SortArray(int[] array)
+        public static T[] SortArray(T[] array)
         {
             for (int i = 0; i < array.Length - 1; i++)
             {
                 for (int j = 0; j < array.Length - i - 1; j++)
                 {
-                    if (array[j] > array[j + 1])
+                    if (array[j].CompareTo(array[j + 1]) >= 0)
                     {
                         var temp = array[j];
                         array[j] = array[j + 1];
