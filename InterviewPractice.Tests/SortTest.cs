d2c4413a45d2095896e477c47b664ffc0784311c
Nathan McGinn <nmcginn@users.noreply.github.com>
2018-04-09 11:22:52 -0500

    merge sort

--- InterviewPractice.Tests/SortTest.cs
+++ InterviewPractice.Tests/SortTest.cs
@@ -21,5 +21,20 @@ namespace InterviewPractice.Tests
             Assert.Equal(6, testData[3]);
             Assert.Equal(11, testData[4]);
         }
+
+        [Fact]
+        public void TestMergeSort()
+        {
+            // Arrange
+            var testData = new int[] { 3, 6, 1, 4, 11 };
+            // Act
+            testData = Sort<int>.MergeSort(testData, 0, testData.Length - 1);
+            // Assert
+            Assert.Equal(1, testData[0]);
+            Assert.Equal(3, testData[1]);
+            Assert.Equal(4, testData[2]);
+            Assert.Equal(6, testData[3]);
+            Assert.Equal(11, testData[4]);
+        }
     }
 }
--- InterviewPractice/Sort.cs
+++ InterviewPractice/Sort.cs
@@ -22,5 +22,68 @@ namespace InterviewPractice
             }
             return array;
         }
+
+        public static T[] MergeSort(T[] array, int left, int right)
+        {
+            if (left < right)
+            {
+                var middle = (left + (right - 1)) / 2;
+                var leftArray = MergeSort(array, left, middle);
+                var rightArray = MergeSort(array, middle + 1, right);
+                var result = Merge(array, left, middle, right);
+                return result;
+            }
+            return array;
+        }
+
+        private static T[] Merge(T[] array, int left, int middle, int right)
+        {
+            var n1 = middle - left + 1;
+            var n2 = right - middle;
+            // create temporary arrays for the left & right sides
+            var leftArray = new T[n1];
+            var rightArray = new T[n2];
+            for (var x = 0; x < n1; x++)
+            {
+                leftArray[x] = array[left + x];
+            }
+            for (var x = 0; x < n2; x++)
+            {
+                rightArray[x] = array[middle + 1 + x];
+            }
+            // merge the 2 temporary arrays together
+            var i = 0;
+            var j = 0;
+            var k = left;
+            while (i < n1 && j < n2)
+            {
+                if (leftArray[i].CompareTo(rightArray[j]) != 1)
+                {
+                    array[k] = leftArray[i];
+                    i++;
+                }
+                else
+                {
+                    array[k] = rightArray[j];
+                    j++;
+                }
+                k++;
+            }
+            // copy the remaining left-hand elements
+            while(i < n1)
+            {
+                array[k] = leftArray[i];
+                i++;
+                k++;
+            }
+            // copy the remaining right-hand elements
+            while(j < n2)
+            {
+                array[k] = rightArray[j];
+                j++;
+                k++;
+            }
+            return array;
+        }
     }
 }
