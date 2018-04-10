c410fb1d77e64fb68c1fd9c16a3a2372cbf51c83
Nathan McGinn <nmcginn@users.noreply.github.com>
2018-04-10 07:54:26 -0700

    fizzbuzz, just because

---
+++ InterviewPractice.Tests/FizzBuzzTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+
+namespace InterviewPractice.Tests
+{
+    public class FizzBuzzTest
+    {
+        [Fact]
+        public void TestFizzBuzz()
+        {
+            // arrange
+            // act
+            var result = FizzBuzz.FizzBuzzer();
+            var results = result.TrimEnd().Split(Environment.NewLine);
+            // assert
+            Assert.Equal(100, results.Length);
+            Assert.Equal("1", results[0]);
+            Assert.Equal("Fizz", results[2]);
+            Assert.Equal("Buzz", results[4]);
+            Assert.Equal("FizzBuzz", results[14]);
+        }
+    }
+}
---
+++ InterviewPractice/FizzBuzz.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+
+namespace InterviewPractice
+{
+    public class FizzBuzz
+    {
+        public static string FizzBuzzer()
+        {
+            var sb = new StringBuilder();
+            for (var i = 1; i <= 100; i++)
+            {
+                var result =
+                    i % 15 == 0 ? "FizzBuzz" :
+                    i % 3 == 0 ? "Fizz" :
+                    i % 5 == 0 ? "Buzz" :
+                    i.ToString();
+                sb.AppendLine(result);
+            }
+            return sb.ToString();
+        }
+    }
+}
